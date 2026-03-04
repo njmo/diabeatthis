48a0244ebd5464113a48b0822f6455512ca5a222
Marcin Klonica <mklonica94@gmail.com>
2026-03-04 16:19:32 +0100

    Refactor notification provider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,43 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
 
 import 'router/observers/router_debug_observer.dart';
 import 'router/providers/app_router_provider.dart';
 import 'router/providers/flutter_local_notifications_plugin_provider.dart';
-
-void initNotifications(WidgetRef ref)  {
-  final flutterLocalNotificationsPlugin = ref.watch(flutterLocalNotificationsPluginProvider);
-
-  tz.initializeTimeZones();
-  tz.setLocalLocation(tz.getLocation('Europe/Warsaw'));
-
-  const androidInit = AndroidInitializationSettings('@mipmap/ic_launcher');
-  const iosInit = DarwinInitializationSettings(
-    requestAlertPermission: true,
-    requestBadgePermission: true,
-    requestSoundPermission: true,
-  );
-
-  const initSettings = InitializationSettings(
-    android: androidInit,
-    iOS: iosInit,
-  );
-
-  flutterLocalNotificationsPlugin.initialize(settings: initSettings);
-}
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(appRouterProvider);
+    ref.watch(notificationsInitProvider);
 
-    initNotifications(ref);
+    final router = ref.watch(appRouterProvider);
 
     return MaterialApp.router(
       title: 'Flutter + auto_route + Riverpod',
--- lib/app/router/providers/flutter_local_notifications_plugin_provider.dart
+++ lib/app/router/providers/flutter_local_notifications_plugin_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 part 'flutter_local_notifications_plugin_provider.g.dart';
 
@@ -8,7 +10,38 @@ FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin(Ref ref)
 {
   final flutterLocalNotificationsPlugin =
   FlutterLocalNotificationsPlugin();
-  flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
-      AndroidFlutterLocalNotificationsPlugin>()?.requestNotificationsPermission();
+
   return flutterLocalNotificationsPlugin;
+}
+
+@Riverpod(keepAlive: true)
+Future<void> notificationsInit(Ref ref) async {
+  final plugin = ref.read(flutterLocalNotificationsPluginProvider);
+
+  tz.initializeTimeZones();
+  tz.setLocalLocation(tz.getLocation('Europe/Warsaw'));
+
+  const androidInit = AndroidInitializationSettings('@mipmap/ic_launcher');
+  const iosInit = DarwinInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+
+  const initSettings = InitializationSettings(
+    android: androidInit,
+    iOS: iosInit,
+  );
+
+  await plugin.initialize(settings: initSettings);
+
+  final android = plugin.resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>();
+
+  await android?.requestNotificationsPermission();
+
+  final canExact = await android?.canScheduleExactNotifications() ?? true;
+  if (!canExact) {
+    await android?.requestExactAlarmsPermission();
+  }
 }
